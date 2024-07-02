7892fb337adb33aa3cb73938d31cacdb060cd327
yashed <yashedthisara2001@gmail.com>
2024-07-02 22:19:23 +0530

    add teacher home page components

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,20 @@
 import loadable from '@loadable/component';
 import { Route, Routes } from 'react-router-dom';
 import StudentRoutes from './routes/student';
+// import TeacherRoutes from './routes/teacher';
 
 const Login = loadable(() => import('@/pages/login'));
 const Register = loadable(() => import('@/pages/register'));
 
 function App() {
-    let logged = false;
+    let logged = true;
 
     return (
         <div className="w-full min-h-screen overflow-x-hidden">
             {logged ? (
                 <StudentRoutes />
             ) : (
+                // <TeacherRoutes />
                 <Routes>
                     <Route path="/" element={<Login />} />
                     <Route path="/signup" element={<Register />} />
