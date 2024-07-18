5405485bceb245e1b6842f5458791475e81c47b7
aneaser <anudaattanayake@gmail.com>
2024-07-18 20:06:24 +0530

    main UIs

---
+++ src/pages/institute/teachers.tsx
@@ -0,0 +1,11 @@
+import Heading from '@/components/headings/main';
+
+function Teachers() {
+    return(
+        <div className="flex flex-col gap-3">
+            <Heading>Teachers</Heading>
+        </div>
+    )
+}
+
+export default Teachers;
--- src/routes/institute.tsx
+++ src/routes/institute.tsx
@@ -1,14 +1,21 @@
 import Layout from '@/layout';
 import loadable from '@loadable/component';
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-import Home from '@/pages/institute/home';
+const Home = loadable(() => import('@/pages/institute/home'));
+const Teachers = loadable(() => import('@/pages/institute/teachers'));
+const Profile = loadable(() => import('@/pages/institute/profile'));
+const Notification = loadable(()  => import('@/pages/institute/notifications'));
 
-
-export default function StudentRoutes() {
+export default function InstituteRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Layout />}>
+                <Route path="" element={<Home />} />
+                <Route path="teachers" element={<Teachers/>} />
+                <Route path="profile" element={<Profile />} />
+                <Route path="notifications" element={<Notification />} />
+            </Route>
         </Routes>
     );
 }
