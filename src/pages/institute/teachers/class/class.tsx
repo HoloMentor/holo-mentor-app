2ac741e8df48bf5357c93e546675fb0e91059efe
aneaser <anudaattanayake@gmail.com>
2024-07-19 07:26:39 +0530

    Teacher Profile

---
+++ src/pages/institute/teachers/class/class.tsx
@@ -0,0 +1,10 @@
+
+function Class() {
+  return (
+    <div>
+      <h1>Class</h1>
+    </div>
+    );
+}
+
+export default Class;
--- src/routes/institute.tsx
+++ src/routes/institute.tsx
@@ -1,20 +1,28 @@
 import Layout from '@/layout';
 import loadable from '@loadable/component';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Outlet } from 'react-router-dom';
 
 const Home = loadable(() => import('@/pages/institute/home'));
 const Teachers = loadable(() => import('@/pages/institute/teachers'));
 const Profile = loadable(() => import('@/pages/institute/profile'));
 const Notification = loadable(()  => import('@/pages/institute/notifications'));
+const TeacherProfile = loadable(() => import('@/pages/institute/teachers/profile'));
+const Class = loadable(() => import('@/pages/institute/teachers/class/class'));
 
 export default function InstituteRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route path="" element={<Home />} />
-                <Route path="teachers" element={<Teachers/>} />
+                <Route path="teachers" element={<Outlet/>}>
+                    <Route path="" element={<Teachers />} />
+                    <Route path=":teacherId" element={<TeacherProfile />} />
+                </Route>
                 <Route path="profile" element={<Profile />} />
                 <Route path="notifications" element={<Notification />} />
+            </Route>
+            <Route path="class" element={<Outlet />}>
+                <Route path=":classId" element={<Class />} />
             </Route>
         </Routes>
     );
