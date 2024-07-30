8562424f6b5c21fb70b2dea8d721cdd9ec2f7203
Kumudu2001 <2021is105@stu.ucsc.cmb.ac.lk>
2024-07-30 17:05:50 +0530

    change staff routes

--- src/routes/staff.tsx
+++ src/routes/staff.tsx
@@ -10,12 +10,12 @@ const StudyPlan = loadable(() => import('@/pages/teacher/study-plan'));
 const CreatePlan = loadable(() => import('@/pages/teacher/study-plan-create'));
 const StudyPlanView = loadable(() => import('@/pages/teacher/study-plan-individual'));
 const ClassMaterials = loadable(() => import('@/pages/teacher/classes/materials'));
-const StudentList = loadable(() => import('@/pages/teacher/home'));
+const StudentList = loadable(() => import('@/pages/teacher/student-list'));
 const Submissions = loadable(() => import('@/pages/teacher/submissions'));
 const StudentProfile = loadable(() => import('@/pages/teacher/student-profile-details'));
 const SubjectForums = loadable(() => import('@/pages/staff/classes/forum/'));
 const SubjectForumMCQ = loadable(() => import('@/pages/staff/classes/forum/mcq'));
-const SubjectForumEssay = loadable(() => import('@/pages/teacher/classes/forum/essay'));
+const SubjectForumEssay = loadable(() => import('@/pages/staff/classes/forum/essay'));
 const SubjectForum = loadable(() => import('@/pages/staff/classes/forum/question'));
 const Quizes = loadable(() => import('@/pages/staff/classes/quiz'));
 const QuizInfo = loadable(() => import('@/pages/staff/classes/quiz/questions'));
@@ -27,32 +27,31 @@ export default function TeacherRoutes() {
                 <Route path="" element={<Home />} />
                 <Route path="classes" element={<Outlet />}>
                     <Route path="" element={<Classes />}></Route>
-                    <Route path=":classId" element={<Outlet />}></Route>
-                    //add routes inside classId
-                </Route>
-                <Route path="forum" element={<SubjectForums />} />
-                <Route path="forum/mcq" element={<SubjectForumMCQ />} />
-                <Route path="forum/essay" element={<SubjectForumEssay />} />
-                <Route path="forum/:forumId" element={<SubjectForum />} />
-                <Route path="quiz/" element={<Quizes />} />
-                <Route path="quiz/:quizId" element={<QuizInfo />} />
-                <Route path="studyplan" element={<Outlet />}>
-                    <Route path="" element={<StudyPlan />}></Route>
-                    <Route path="create" element={<CreatePlan />}></Route>
-                    <Route path="view" element={<StudyPlanView />}></Route>
-                </Route>
-                <Route path="student" element={<Outlet />}>
-                    <Route path="" element={<StudentList />}></Route>
-                    <Route path="details"></Route>
-                    <Route path="submissions" element={<Submissions />}></Route>
-                    <Route path="profile" element={<StudentProfile />}></Route>
+                    <Route path=":classId" element={<Outlet />}>
+                        <Route path="" element={<ClassMaterials />} />
+                        <Route path="forum" element={<SubjectForums />} />
+                        <Route path="forum/mcq" element={<SubjectForumMCQ />} />
+                        <Route path="forum/essay" element={<SubjectForumEssay />} />
+                        <Route path="forum/:forumId" element={<SubjectForum />} />
+                        <Route path="quiz/" element={<Quizes />} />
+                        <Route path="quiz/:quizId" element={<QuizInfo />} />
+                        <Route path="studyplan" element={<Outlet />}>
+                            <Route path="" element={<StudyPlan />}></Route>
+                            <Route path="create" element={<CreatePlan />}></Route>
+                            <Route path="view" element={<StudyPlanView />}></Route>
+                        </Route>
+                        <Route path="student" element={<Outlet />}>
+                            <Route path="" element={<StudentList />}></Route>
+                            <Route path="details"></Route>
+                            <Route path="submissions" element={<Submissions />}></Route>
+                            <Route path="profile" element={<StudentProfile />}></Route>
+                        </Route>
+                    </Route>
                 </Route>
 
                 <Route path="profile" element={<Profile />} />
                 <Route path="notifications" element={<Notifications />} />
-
-                <Route path=":institute/:year/*" element={<ClassMaterials />}></Route>
             </Route>
         </Routes>
-    );
+        );
 }
