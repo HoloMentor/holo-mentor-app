c8f49aafb93fdcecd4683123105d40176f2a8d1f
senunn” <senuxxx123@gmail.com>
2024-08-25 14:28:28 +0530

    study plan view and study plan tier changed

--- src/pages/teacher/study-plan.tsx
+++ src/pages/teacher/study-plan.tsx
@@ -1,36 +1,16 @@
-import { useState } from 'react';
 import StudyPlanCard from '@/components/cards/study-plan-card';
-import { Button, Popover, PopoverTrigger, PopoverContent } from '@nextui-org/react';
+import { Tabs, Tab, Card, CardBody} from '@nextui-org/react';
+import Button from '@/components/button';
 import { useDispatch } from 'react-redux';
-import { modelNames } from '@/models';
 import { modelActions } from '@/redux/reducers/model.reducer';
-import { Tabs, Tab, Card, CardBody, CardHeader } from '@nextui-org/react';
+import { modelNames } from '@/models';
 
 export default function StudyPlan() {
-    const [toggleState, setToggleState] = useState<number>(1);
     const dispatch = useDispatch();
-
-    let tabs = [
-        {
-          id: "photos",
-          label: "Photos",
-          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        },
-        {
-          id: "music",
-          label: "Music",
-          content: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-        },
-        {
-          id: "videos",
-          label: "Videos",
-          content: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        }
-      ];
-
     const tiers = [
         {
             id: 1,
+            label: 'Tier 1',
             content: [
                 <StudyPlanCard planName="Plan for Tier 1" autherName="Amith Pussella" />,
                 <StudyPlanCard planName="Plan for Tier 1B" autherName="Amith Pussella" />
@@ -39,6 +19,7 @@ export default function StudyPlan() {
         },
         {
             id: 2,
+            label: 'Tier 2',
             content: [
                 <StudyPlanCard planName="Plan for Tier 2" autherName="Author 1" />,
                 <StudyPlanCard planName="Plan for Tier 2" autherName="Author 1" />,
@@ -48,38 +29,57 @@ export default function StudyPlan() {
         },
         {
             id: 3,
+            label: 'Tier 3',
             content: [<StudyPlanCard planName="Plan for Tier 3" autherName="Author 1" />],
             tierDescription: 'Tier 03: Marks 40 - 60'
         },
         {
             id: 4,
+            label: 'Tier 4',
             content: [<StudyPlanCard planName="Plan for Tier 4" autherName="Author 1" />],
             tierDescription: 'Tier 04: Marks 20 - 40'
         },
         {
             id: 5,
+            label: 'Tier 5',
             content: [<StudyPlanCard planName="Plan for Tier 5" autherName="Author 1" />],
             tierDescription: 'Tier 05: Marks 0 - 20'
         }
     ];
-
-    const buttonClasses = (tier: number): string =>
-        `p-4 rounded-t-lg shadow-lg flex  justify-between ${
-            toggleState === tier ? 'bg-white' : 'bg-gray-200'
-        } font-semibold text-xl`;
 
     return (
         <section className="h-screen mb-20">
             <div className="flex p-4 mr-4 bg-white rounded-md shadow-md">
                 <p className="ml-3 text-3xl font-bold text-dark-green">Study Plan</p>
             </div>
-            <section className='my-3 mr-3'>
+            <section className="my-2 mr-2">
                 <div className="flex flex-col w-full">
-                    <Tabs aria-label="Dynamic tabs" items={tabs}>
+                    <Tabs aria-label="Dynamic tabs" items={tiers}>
                         {(item) => (
-                            <Tab key={item.id} title={item.label}>
-                                <Card className='py-4 rounded-md'>
-                                    <CardBody >{item.content}</CardBody>
+                            <Tab
+                                key={item.id}
+                                title={item.label}
+                                className="py-5 font-semibold px-7 text-medium">
+                                <Card className="rounded-md ">
+                                    <div className="flex justify-between px-8 py-2 mt-4">
+                                        <p className='text-xl text-dark-green'>Current Study Plan</p>
+                                        <Button onClick={() =>
+                            dispatch(
+                                modelActions.show({
+                                    name: modelNames.ADD_STUDY_PLAN
+                                })
+                            )
+                        }>Add New Plan</Button>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <CardBody className="grid grid-cols-2 gap-4">
+                                            {item.content.map((card, index) => (
+                                                <div key={index} className='py-4'>
+                                                    {card}
+                                                </div>
+                                            ))}
+                                        </CardBody>
+                                    </div>
                                 </Card>
                             </Tab>
                         )}
