34ac1766759a8b9b48c253c94fe6b10f933e5d60
senunn” <senuxxx123@gmail.com>
2024-11-29 15:32:48 +0530

    quiz question activation deactivation

--- src/pages/teacher/classes/quiz/quiz-bank-view.tsx
+++ src/pages/teacher/classes/quiz/quiz-bank-view.tsx
@@ -13,7 +13,6 @@ import {
     DropdownSection,
     DropdownTrigger
 } from '@nextui-org/react';
-import { any } from 'prop-types';
 
 export default function Forum() {
     const { classId } = useParams();
@@ -32,6 +31,27 @@ export default function Forum() {
         }
     );
     useErrorHandler(isClassTopicsError, classTopicsError);
+
+    const [deActivateQuestion] = questionServices.useDeActivateeQuestionMutation();
+    const [activateQuestion] = questionServices.useActivateeQuestionMutation();
+
+    const handleActivate = async (id: number) => {
+        try {
+            await activateQuestion({ id }).unwrap();
+            console.log(`Question with id ${id} deactivated successfully`);
+        } catch (error) {
+            console.error('Failed to deactivate question:', error);
+        }
+    };
+
+    const handleDeactivate = async (id: number) => {
+        try {
+            await deActivateQuestion({ id }).unwrap();
+            console.log(`Question with id ${id} deactivated successfully`);
+        } catch (error) {
+            console.error('Failed to deactivate question:', error);
+        }
+    };
 
     const classTopicsData = useMemo(() => {
         return (
@@ -58,7 +78,6 @@ export default function Forum() {
     );
 
     useErrorHandler(isQuizQuestionsError, quizQuestionsError);
-    console.log('OK');
 
     return (
         <div className="flex flex-col gap-3">
@@ -66,7 +85,13 @@ export default function Forum() {
 
             <section className="flex flex-col gap-5 pr-5">
                 {quizQuestions?.data?.map(
-                    (_: { id: number; question: any; subTopic: number; mcqAnswer: any }) => {
+                    (_: {
+                        id: number;
+                        question: any;
+                        subTopic: number;
+                        mcqAnswer: any;
+                        activation: number;
+                    }) => {
                         const topicNumber = Number(_.subTopic);
                         const topic = classTopicsData.find(
                             (topic: { value: number }) => topic.value === topicNumber
@@ -74,6 +99,7 @@ export default function Forum() {
                         console.log(typeof _.subTopic);
                         console.log(topic);
                         console.log('Answers:', _.mcqAnswer);
+                        console.log('Activation:', _.activation);
 
                         return (
                             <div key={_.id} className="flex gap-3 p-6 bg-white rounded-md">
@@ -81,6 +107,11 @@ export default function Forum() {
                                     <h3 className="text-lg font-semibold">
                                         {topic ? topic.label : 'Unknown Topic'}
                                     </h3>
+                                    {_.activation === 1 && (
+                                        <span className="p-4 text-lg bg-red-200 rounded-md text-danger">
+                                            Inactive
+                                        </span>
+                                    )}
                                     <Reader value={_.question} />
                                     <ul className="space-y-2">
                                         {_.mcqAnswer?.map(
@@ -140,27 +171,39 @@ export default function Forum() {
                                                 </DropdownItem>
                                             </DropdownSection>
                                             <DropdownSection>
-                                                <DropdownItem
-                                                    endContent={
-                                                        <svg
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                            fill="none"
-                                                            viewBox="0 0 24 24"
-                                                            strokeWidth={1.5}
-                                                            stroke="currentColor"
-                                                            className="size-4">
-                                                            <path
-                                                                strokeLinecap="round"
-                                                                strokeLinejoin="round"
-                                                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                                                            />
-                                                        </svg>
-                                                    }
-                                                    className="text-danger"
-                                                    key="delete">
-                                                    Delete
-                                                </DropdownItem>
+                                                {_.activation === 0 && (
+                                                    <DropdownItem
+                                                        onClick={() => handleDeactivate(_.id)}
+                                                        endContent={
+                                                            <svg
+                                                                xmlns="http://www.w3.org/2000/svg"
+                                                                fill="none"
+                                                                viewBox="0 0 24 24"
+                                                                strokeWidth={1.5}
+                                                                stroke="currentColor"
+                                                                className="size-4">
+                                                                <path
+                                                                    strokeLinecap="round"
+                                                                    strokeLinejoin="round"
+                                                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                                                />
+                                                            </svg>
+                                                        }
+                                                        className="text-danger"
+                                                        key="delete">
+                                                        Deactivate
+                                                    </DropdownItem>
+                                                )}
+                                                {_.activation === 1 && (
+                                                    <DropdownItem
+                                                        onClick={() => handleActivate(_.id)}
+                                                        className="text-black"
+                                                        key="delete">
+                                                        Activate
+                                                    </DropdownItem>
+                                                )}
                                             </DropdownSection>
+                                            
                                         </DropdownMenu>
                                     </Dropdown>
                                 </>
